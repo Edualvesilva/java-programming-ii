478453e3a819c3c89b616aedd6a88aeec9ecd8f4
Eduardo Alves da Silva <eduardoalvniver@gmail.com>
2025-06-17 22:04:16 -0300

    08-01 solved

--- part08-Part08_01.Cubes/src/main/java/Cubes.java
+++ part08-Part08_01.Cubes/src/main/java/Cubes.java
@@ -5,6 +5,27 @@ public class Cubes {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        boolean isInteger = false;
+
+        int cube = 0;
+        while (true) {
+            String userString = scanner.nextLine();
+
+            if (userString.equals("end")) {
+
+                break;
+            }
+
+            isInteger = true;
+
+            if (isInteger == true) {
+                int number = Integer.valueOf(userString);
+
+                cube = number * number * number;
+                System.out.println(cube);
+            }
+
+        }
 
     }
 }
