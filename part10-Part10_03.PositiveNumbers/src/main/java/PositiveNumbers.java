7e2d0764a37baf3376d3d2d84a9e0c55b7f23836
Eduardo Alves da Silva <eduardoalvniver@gmail.com>
2025-07-20 19:39:54 -0300

    part 10-03

--- part10-Part10_03.PositiveNumbers/src/main/java/PositiveNumbers.java
+++ part10-Part10_03.PositiveNumbers/src/main/java/PositiveNumbers.java
@@ -1,5 +1,6 @@
 
 import java.util.List;
+import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.stream.Collectors;
 
@@ -8,7 +9,19 @@ public class PositiveNumbers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         // test your method here
+        List<Integer> list = new ArrayList<>();
+        list.add(3);
+        list.add(7);
+        list.add(4);
+        list.add(2);
+        list.add(6);
+        positive(list);
+    }
 
+    public static List<Integer> positive(List<Integer> numbers) {
+        List positive = numbers.stream().filter(number -> number >= 0).collect(Collectors.toCollection(ArrayList::new));
+
+        return positive;
     }
 
 }
