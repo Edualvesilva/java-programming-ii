3bba0d84c78ad55b4f83cde2281289be0bdba42b
Eduardo Alves da Silva <eduardoalvniver@gmail.com>
2025-07-20 19:10:04 -0300

    part 10-02

--- part10-Part10_02.AverageOfSelectedNumbers/src/main/java/AverageOfSelectedNumbers.java
+++ part10-Part10_02.AverageOfSelectedNumbers/src/main/java/AverageOfSelectedNumbers.java
@@ -8,5 +8,33 @@ public class AverageOfSelectedNumbers {
         Scanner scanner = new Scanner(System.in);
         // toteuta ohjelmasi tänne
 
+        ArrayList<String> list = new ArrayList<>();
+
+        while (true) {
+            System.out.println("Input numbers, type \"end\" to stop.");
+            String row = scanner.nextLine();
+
+            if (row.equals("end")) {
+                break;
+            }
+
+            list.add(row);
+
+        }
+
+        System.out.println("Print the average of the negative numbers or the positive numbers? (n/p)");
+        String answer = scanner.nextLine();
+
+        if (answer.equals("n")) {
+            double average = list.stream().mapToInt(s -> Integer.valueOf(s)).filter(number -> number < 0).average().getAsDouble();
+            System.out.println("Average of the negative numbers: " + average);
+        }
+
+        if (answer.equals("p")) {
+            double average = list.stream().mapToInt(s -> Integer.valueOf(s)).filter(number -> number >= 0).average().getAsDouble();
+            System.out.println("Average of the positive numbers: " + average);
+
+        }
+
     }
 }
