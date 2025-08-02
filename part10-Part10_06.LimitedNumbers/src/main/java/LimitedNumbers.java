91252deb27a1c234496664f3c9860de048ef2ffe
Eduardo Alves da Silva <eduardoalvniver@gmail.com>
2025-08-02 16:01:55 -0300

    part 10-06

--- part10-Part10_06.LimitedNumbers/src/main/java/LimitedNumbers.java
+++ part10-Part10_06.LimitedNumbers/src/main/java/LimitedNumbers.java
@@ -6,6 +6,22 @@ public class LimitedNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        ArrayList<Integer> list = new ArrayList<>();
 
+        while(true){
+        int number = scanner.nextInt();
+        
+        if(number < 0){
+        break;
+        }
+        
+        list.add(number);
+        
+        
+        }
+   
+        list.stream().filter(number -> number > 0 && number <= 5).forEach(value -> System.out.println(value));
+        
+        
     }
 }
