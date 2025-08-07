4d58b4ade73ce227adae4bd033eaa7d9e9eeaba1
Eduardo Alves da Silva <eduardoalvniver@gmail.com>
2025-08-07 20:59:07 -0300

    part 10-13

--- part10-Part10_13.LiteracyComparison/src/main/java/LiteracyComparison.java
+++ part10-Part10_13.LiteracyComparison/src/main/java/LiteracyComparison.java
@@ -5,8 +5,14 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 
 public class LiteracyComparison {
-    
+
     public static void main(String[] args) {
+
+        try {
+            Files.lines(Paths.get("literacy.csv")).map(row -> row.split(",")).sorted((x, y) -> x[5].compareTo(y[5])).forEach(row -> System.out.println(row[3] + " (" + row[4] + "), " + row[2].split(" ")[1].trim() + ", " + row[5]));
+        } catch (Exception e) {
+            System.out.println("error: " + e.getMessage());
+        }
 
     }
 }
