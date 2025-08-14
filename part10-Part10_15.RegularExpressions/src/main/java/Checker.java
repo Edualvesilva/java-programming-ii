ff8c9a357c71555b5851927c8c96f4649f7043b3
Eduardo Alves da Silva <eduardoalvniver@gmail.com>
2025-08-14 20:45:09 -0300

    part 10-15

--- part10-Part10_15.RegularExpressions/src/main/java/Checker.java
+++ part10-Part10_15.RegularExpressions/src/main/java/Checker.java
@@ -1,5 +1,38 @@
 
-
 public class Checker {
 
+    private String daysWeek;
+    private String allVowels;
+    private String hours;
+    
+    
+    public Checker() {
+        this.daysWeek = "(mon|tue|wed|thu|fri|sat|sun)";
+        this.allVowels = "[aeiou]+";
+        this.hours = "(0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]:[0-5][0-9]";
+    }
+
+    public boolean isDayOfWeek(String string) {
+        if (string.matches(this.daysWeek)) {
+            return true;
+
+        } else {
+            return false;
+        }
+    }
+
+    public boolean allVowels(String string) {
+        if (string.matches(this.allVowels)) {
+            return true;
+        }
+        return false;
+    }
+    
+    
+    public boolean timeOfDay(String string){
+    if(string.matches(hours)){
+    return true;
+    }
+    return false;
+    }
 }
