d3c77847ad25bc8c959a13de6ebc981c88049283
Eduardo Alves da Silva <eduardoalvniver@gmail.com>
2025-08-18 20:30:34 -0300

    part 11-05

---
+++ part11-Part11_05.ThePlayerAndTheBot/src/main/java/Bot.java
@@ -0,0 +1,8 @@
+
+public class Bot extends Player{
+ 
+    private void play(){}
+    
+    private void addMove(String move){}
+    
+}
---
+++ part11-Part11_05.ThePlayerAndTheBot/src/main/java/Player.java
@@ -0,0 +1,8 @@
+
+public class Player {
+    private String name;
+    
+    private void play(){}
+    
+    private void printName(){}
+}
