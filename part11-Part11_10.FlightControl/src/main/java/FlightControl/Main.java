8c56c3569310feee9a20f5a8b52c52632544328b
Eduardo Alves da Silva <eduardoalvniver@gmail.com>
2025-08-21 20:59:56 -0300

    part 11-10

--- part11-Part11_10.FlightControl/src/main/java/FlightControl/Main.java
+++ part11-Part11_10.FlightControl/src/main/java/FlightControl/Main.java
@@ -1,9 +1,15 @@
 package FlightControl;
-
+import FlightControl.ui.TextUi;
+import java.util.Scanner;
+import FlightControl.logic.FlightControl;
 
 public class Main {
 
     public static void main(String[] args) {
         // Write the main program here. It is useful to create some classes of your own.
+        Scanner scanner = new Scanner(System.in);
+        FlightControl flightControl = new FlightControl();
+        TextUi textui = new TextUi(flightControl,scanner);
+        textui.start();
     }
 }
