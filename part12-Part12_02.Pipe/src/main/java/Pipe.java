3a37e1ae2b3990d019954bc4df30714776c9650b
Eduardo Alves da Silva <eduardoalvniver@gmail.com>
2025-09-09 19:52:57 -0300

    part 12-02

---
+++ part12-Part12_02.Pipe/src/main/java/Pipe.java
@@ -0,0 +1,37 @@
+
+import java.util.ArrayList;
+
+public class Pipe<T> {
+
+    private ArrayList<T> pipe;
+
+    public Pipe() {
+        this.pipe = new ArrayList<>();
+    }
+
+    public void putIntoPipe(T value) {
+
+        this.pipe.add(value);
+
+    }
+
+    public T takeFromPipe() {
+        if (this.pipe.isEmpty()) {
+            return null;
+        }
+
+        T saveObject = this.pipe.get(0);
+        this.pipe.remove(saveObject);
+        return saveObject;
+    }
+
+    public boolean isInPipe() {
+        if (!this.pipe.isEmpty()) {
+            return true;
+        }
+
+        return false;
+
+    }
+
+}
