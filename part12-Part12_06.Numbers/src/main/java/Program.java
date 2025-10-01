497a3d7f443562a8f2e8adb0377562cb5e6a2c38
Eduardo Alves da Silva <eduardoalvniver@gmail.com>
2025-10-01 19:41:21 -0300

    part 12 - 06

--- part12-Part12_06.Numbers/src/main/java/Program.java
+++ part12-Part12_06.Numbers/src/main/java/Program.java
@@ -6,6 +6,18 @@ public class Program {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        Random luck = new Random();
+
+        System.out.println("How many random numbers should be printed? ");
+
+        int userNumber = Integer.valueOf(scanner.nextLine());
+
+        for (int i = 0; i <= userNumber - 1; i++) {
+            int randomNumbers = luck.nextInt(11);
+            System.out.println(randomNumbers);
+
+        }
+
     }
 
 }
